3621d9e08411ddba5cc33d30902092cab7914c50
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-02-11 22:22:11 +0000

    queries.js: Wrote and exported functions getAllExecutives, postNewExecutive, deleteExecutive, getAllCampaigns, postNewCampaign, deleteCampaign, getAllTypesOfClient

---
+++ db/queries.js
@@ -0,0 +1,42 @@
+const pool = require("./pool");
+
+async function getAllExecutives() {
+    const { rows } = await pool.query("SELECT * FROM executive");
+    return rows;
+}
+
+async function postNewExecutive(firstName, lastName) {
+    await pool.query("INSERT INTO executive (first_name, last_name) VALUES ($1, $2)", [firstName, lastName]);
+}
+
+async function deleteExecutive(id) {
+    await pool.query("DELETE FROM executive WHERE id = $1", [id]);
+}
+
+async function getAllCampaigns() {
+    const { rows } = await pool.query("SELECT * FROM campaign");
+    return rows;
+}
+
+async function postNewCampaign(brand) {
+    await pool.query("INSERT INTO campaign (brand) VALUES $1", [brand]);
+}
+
+async function deleteCampaign(id) {
+    await pool.query("DELETE FROM campaign WHERE id = $1", [id]);
+}
+
+async function getAllTypesOfClient() {
+    const { rows } = await pool.query("SELECT * FROM type_of_client");
+    return rows;
+}
+
+module.exports = {
+    getAllExecutives,
+    postNewExecutive,
+    deleteExecutive,
+    getAllCampaigns,
+    postNewCampaign,
+    deleteCampaign,
+    getAllTypesOfClient,
+};
