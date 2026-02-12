63927315e27380c860dbebb5bd41267368c2f44c
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-02-12 02:18:06 +0000

    app.js: Imported sterlingCooperRouter and set app to use it

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("node:path");
 const app = express();
+const sterlingCooperRouter = require("./routes/sterlingCooperRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use("/", sterlingCooperRouter);
 
 
 const PORT = process.env.PORT || 3001;
