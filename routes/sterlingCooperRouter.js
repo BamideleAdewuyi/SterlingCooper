260557341cb46c184277c290074394ffb6214f6d
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-02-12 02:09:59 +0000

    sterlingCooperRouter.js: Imported sterlingCooperController and set routes for all functions

---
+++ routes/sterlingCooperRouter.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const sterlingCooperController = require("../controllers/sterlingCooperController");
+const sterlingCooperRouter = Router();
+
+// Get
+sterlingCooperRouter.get("/executives", sterlingCooperController.executivesGet);
+sterlingCooperRouter.get("/campaigns", sterlingCooperController.campaignsGet);
+sterlingCooperRouter.get("/types", sterlingCooperController.allTypesGet);
+
+// Post
+sterlingCooperRouter.post("/executives", sterlingCooperController.newExecutivePost);
+sterlingCooperRouter.post("/campaigns", sterlingCooperController.newCampaignPost);
+
+// Delete
+sterlingCooperRouter.delete("/executive/:id", sterlingCooperController.deleteExecutive);
+sterlingCooperRouter.delete("/campaigns/:id", sterlingCooperController.deleteCampaign);
+
+module.exports = sterlingCooperRouter;
