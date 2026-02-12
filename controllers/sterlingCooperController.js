4ae931b6125c9956583a8cbce61c186c5d03edc0
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-02-12 01:30:34 +0000

    sterlingCooperController.js: Imported queries from queries.js and wrote them as async controller functions. Exported

---
+++ controllers/sterlingCooperController.js
@@ -0,0 +1,59 @@
+const db = require("../db/queries");
+
+async function executivesGet(req, res) {
+  const executives = await db.getAllExecutives();
+  res.render("executives", {
+        title: "All Executives",
+        executives: executives,
+  });
+};
+
+async function newExecutivePost(req, res) {
+    const { firstName, lastName } = req.body;
+    await db.postNewExecutive({ firstName, lastName });
+    res.redirect("executives");
+};
+
+async function deleteExecutive(req, res) {
+    const { id } = req.body;
+    await db.deleteExecutive({ id });
+    res.redirect("executives");
+};
+
+async function campaignsGet(req, res) {
+    const campaigns = await db.getAllCampaigns();
+    res.render("campaigns", {
+        title: "All Campaigns",
+        campaigns: campaigns,
+    });
+};
+
+async function newCampaignPost(req, res) {
+    const { brand } = req.body;
+    await db.postNewCampaign({ brand });
+    res.redirect("campaigns");
+};
+
+async function deleteCampaign(req, res) {
+    const { id } = req.body;
+    await db.deleteCampaign({ id });
+    res.redirect("campaigns");
+};
+
+async function getAllTypes(req, res) {
+    const types = await db.getAllTypesOfClient();
+    res.render("types", {
+        title: "All Types of Campaign",
+        types: types,
+    });
+};
+
+module.exports = {
+    executivesGet,
+    newExecutivePost,
+    deleteExecutive,
+    campaignsGet,
+    newCampaignPost,
+    deleteCampaign,
+    getAllTypes,
+};
