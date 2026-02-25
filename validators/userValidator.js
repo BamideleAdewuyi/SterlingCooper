4262beb41378bca4f8dfc2da1f8a8610663b9cd0
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-02-25 00:06:00 +0000

    userValidator.js: Validator now checks that first  name and last name are not empty and contain only letters and spaces

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -1,13 +1,16 @@
 const { body, validationResult, matchedData } = require("express-validator");
 
-const alphaErr = "must only contain letters";
+const alphaErr = "must only contain letters and spaces";
+const lengthErr = "cannot be blank";
 
 
 const validateUser = [
     body("firstName").trim()
-        .isAlpha().withMessage(`First name ${alphaErr}`),
+        .matches(/^[a-zA-Z\s]+$/, 'i').withMessage(`First name ${alphaErr}`)
+        .isLength({ min: 1 }).withMessage(`First name ${lengthErr}`),
     body("lastName").trim()
-        .isAlpha().withMessage(`Last name ${alphaErr}`),
+        .matches(/^[a-zA-Z\s]+$/, 'i').withMessage(`Last name ${alphaErr}`)
+        .isLength({ min: 1 }).withMessage(`Last name ${lengthErr}`),
 ];
 
 module.exports = validateUser;
