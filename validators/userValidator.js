ef1da3762bd7c492f96281e24e96d455f8d9be3c
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-02-25 14:36:56 +0000

    userValidator.js: Added validation for brand and CorporateOrCharity. Added .optional to all fields

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -5,12 +5,20 @@ const lengthErr = "cannot be blank";
 
 
 const validateUser = [
-    body("firstName").trim()
+    body("firstName").optional()
+        .trim()
         .matches(/^[a-zA-Z\s]+$/, 'i').withMessage(`First name ${alphaErr}`)
         .isLength({ min: 1 }).withMessage(`First name ${lengthErr}`),
-    body("lastName").trim()
+    body("lastName").optional()
+        .trim()
         .matches(/^[a-zA-Z\s]+$/, 'i').withMessage(`Last name ${alphaErr}`)
         .isLength({ min: 1 }).withMessage(`Last name ${lengthErr}`),
+    body("brand").optional()
+        .trim()
+        .matches(/^[a-zA-Z\s]+$/, 'i').withMessage(`Brand name ${alphaErr}`)
+        .isLength({ min: 1 }).withMessage(`Brand name ${lengthErr}`),
+    body("CorporateOrCharity").trim()
+        .isLength({ min: 1}).withMessage(`Corporate or charity ${lengthErr}`),
 ];
 
 module.exports = validateUser;
